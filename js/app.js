3ee30fe3237c9ef53276a60d770fb1c3807186d1
PattyOlvera <paty.orion@outlook.com>
2022-04-29 11:27:04 -0500

    Create array with notes logic added

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 class App {
     constructor() {
+        this.notes = [];
         this.$form = document.querySelector('#form');
         this.$noteTitle = document.querySelector('#note-title');
+        this.$noteText = document.querySelector('#note-text');
         this.$formButtons = document.querySelector('#form-buttons');
         this.addEventListeners();
     }
@@ -9,6 +11,18 @@ class App {
     addEventListeners() {
         document.body.addEventListener('click', (event) => {
             this.handleFormClick(event);
+        });
+
+        this.$form.addEventListener('submit', event => {
+            event.preventDefault();
+            const title = this.$noteTitle.value;
+            const text = this.$noteText.value;
+            // we evaluate that at least one of the fields is not empty
+            const hasNote = title || text;
+            if(hasNote){
+                // add Note
+                this.addNote({title, text});
+            }            
         });
     }
 
@@ -35,8 +49,16 @@ class App {
         this.$formButtons.style.display = 'none';
     }
 
-
-
+    addNote(note) {
+        const newNote = {
+            title: note.title,
+            text: note.text,
+            color: 'white',
+            id: this.notes.length > 0 ? this.notes[this.notes.length - 1].id + 1 : 1
+        };
+        this.notes = [...this.notes, newNote];
+        console.log(this.notes);
+    }
 }
 
 new App();
